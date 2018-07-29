b33637123394e9262866b4d1b79f441e60a57ef9
ste ste <steelst0@gmail.com>
2018-07-29 13:46:12 -0700

    initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Human_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
